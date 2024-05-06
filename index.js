dd1fb9c222a9d5b833c4b1f44dc35e7a087b609c
Froguinho69 <lrodrigues@iesb.edu.br>
2024-05-06 17:52:54 -0300

    subindo git

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const pessoas = require('./routes/pessoas')
+app.use(pessoas)
+
+app.listen(3000, () =>{
+    console.log("Aplicação rodando.")
+    
+})
